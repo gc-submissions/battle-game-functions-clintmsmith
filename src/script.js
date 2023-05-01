48f1de9b37c18e4158f73efa615850da2ab0e84a
clintmsmith <clintsmith@rocketmortgage.com>
2023-05-01 11:03:40 -0700

    updating

--- src/script.js
+++ src/script.js
@@ -61,14 +61,14 @@ function fight(player1, player2, player1Health, player2Health) {
         if (attacker === player1) {
             player2Health = attackPlayer(player2Health);
             logHealth(player2, player2Health)
-            if (isDead(player2Health)) {
+            if (isDead(player2Health) === true) {
                 logDeath(player1, player2);
                 break;
             }
         } else {
             player1Health = attackPlayer(player1Health);
             logHealth(player1, player1Health)
-            if (isDead(player1Health)) {
+            if (isDead(player1Health) === true) {
                 logDeath(player2, player1);
                 break;
             }
